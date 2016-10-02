533dd37ae4de4c6204da8e011f5af6b4fb16a3af
Chirayu Desai <chirayudesai1@gmail.com>
2016-10-02 14:50:03 +0200

    bluetooth: Our kernel is missing CLOCK_BOOTTIME_ALARM  (alarmtimer)
    * Kernel is still using drivers/rtc/alarm.c, instead of the newer
      kernel/time/alarmtimer.c
    
    Change-Id: Ib8996315592981e3db79f941d7c5b51a3a9eb6b1

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,4 +20,7 @@
 #define BTM_DEF_LOCAL_NAME "Lenovo K5"
 #define BLUETOOTH_QTI_SW TRUE
 #define BLE_VND_INCLUDED TRUE
+
+/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 #endif
